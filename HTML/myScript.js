c9b11a7536feafcbc747ebd52d188429932850c4
nandakrishna75 <44091105+nandakrishna75@users.noreply.github.com>
2019-04-24 23:09:29 +0530

    Changes for nurse page

--- HTML/myScript.js
+++ HTML/myScript.js
@@ -32,3 +32,21 @@ function doctor_forms(key) {
         document.getElementById("pass_form").style.display = "block";
     }
 }
+
+function nurse_forms(key) {
+    if (key == 1) {
+        document.getElementById("nurse_details").style.display = "block";
+        document.getElementById("info_form").style.display = "none";
+        document.getElementById("pass_form").style.display = "none";
+    }
+    else if (key == 2) {
+        document.getElementById("nurse_details").style.display = "none";
+        document.getElementById("info_form").style.display = "block";
+        document.getElementById("pass_form").style.display = "none";
+    }
+    else if (key == 3) {
+        document.getElementById("nurse_details").style.display = "none";
+        document.getElementById("info_form").style.display = "none";
+        document.getElementById("pass_form").style.display = "block";
+    }
+}
