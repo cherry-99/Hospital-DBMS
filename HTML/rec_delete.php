c275bcac83e3a46f16038eca9bc1181bdc0c8a4d
adikiran <adithyakiran1999@gmail.com>
2019-04-27 10:51:17 +0530

    ........

--- HTML/rec_delete.php
+++ HTML/rec_delete.php
@@ -7,18 +7,55 @@ $credentials = "user = postgres password=15739";
 $db = pg_connect("$host $port $dbname $credentials");
 $errors1 = array();
 
-if(isset($_POST["update_info"]))
+if(isset($_POST["remove_emp"]))
 {
-    $pat_id = $_POST["pat_id"];
-    $diagnosis=$_POST["diag"];
-    $med_id = $_POST["med_id"];
-    $query = "UPDATE patient SET diagnosis = '".$diagnosis."' WHERE pat_id = $pat_id";
-    $query2 = "UPDATE medication SET med_id = '".$med_id."' WHERE pat_id = $pat_id";
+    $emp_id = $_POST["emp_id"];
+    $query = "SELECT * FROM HOSPITAL_EMPLOYEE WHERE emp_id='".$emp_id."';";
     $result = pg_query($db,$query);
-    $result2 = pg_query($db,$query2);
-    header("location: doctor.php");
+    $ans = pg_fetch_assoc($result);
+    if($ans)
+    {
+        $emp_type=pg_fetch_result(pg_query("SELECT emp_type FROM HOSPITAL_EMPLOYEE WHERE emp_id='".$emp_id."';"),0,0);
+        $query = "DELETE FROM HOSPITAL_EMPLOYEE WHERE emp_id='".$emp_id."';";
+        $result = pg_query($db,$query);
+        if($emp_type=="DOCTOR")
+        {
+            echo '<script language="javascript">';
+            echo 'alert("PLEASE UPDATE THE PATIENT DOCTOR DATA UNDER CHANGE PAT_DOC TABS")';
+            echo '</script>';
+        }
+        if($emp_type=="NURSE")
+        {
+            echo '<script language="javascript">';
+            echo 'alert("PLEASE UPDATE THE ROOM INCHARGE IN NURSE TAB UNDER THE CHANGE TAB")';
+            echo '</script>';
+        }
+    }
+    else
+    {
+        echo '<script language="javascript">';
+        echo 'alert("EMPLOYEE NOT FOUND IN DATABASE")';
+        echo '</script>';
+    } 
 }
 
+if(isset($_POST["remove_med_inv"]))
+{
+    $med_id = $_POST["med_id"];
+    $query = "SELECT * FROM MEDICINE_INVENTORY WHERE med_id='".$med_id."';";
+    $result = pg_query($db,$query);
+    $ans = pg_fetch_assoc($result);
+    if($ans)
+    {
+        $query = "DELETE FROM MEDICINE_INVENTORY WHERE med_id='".$med_id."';";
+        $result = pg_query($db,$query);
+    }
+    else
+    {
+        echo '<script language="javascript">';
+        echo 'alert("MEDICINE NOT FOUND IN DATABASE")';
+        echo '</script>'
+    }
 ?>
 
 <!DOCTYPE html>
@@ -33,9 +70,6 @@ if(isset($_POST["update_info"]))
     <script src="myScript.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-
-
 </head>
 
 <body>
@@ -74,6 +108,31 @@ if(isset($_POST["update_info"]))
         <div class="tab-content">
             <div id="emp" class="tab-pane fade in active">
                 <h3>Employee</h3>
+                <?php 
+                    $host = "host = localhost";
+                    $port = "port = 5432";
+                    $dbname = "dbname = test";
+                    $credentials = "user = postgres password=15739";
+                    $db = pg_connect("$host $port $dbname $credentials");
+                    $query="SELECT * FROM HOSPITAL_EMPLOYEE";
+                    $result = pg_query($db,$query);
+                    echo '<table id="table1" class="table table-bordered table-striped" border="1" cellpadding="5" align="center">';
+                    echo "<thead><tr><th>EMPLOYEE_ID</th><th>EMPLOYEE_NAME</th><th>GENDER</th><th>AGE</th><th>EMP_TYPE</th><th>SALARY</th><th>CONTACT_NO</th></tr></thead><tbody>";
+                    // loop through results of database query, displaying them in the table
+                    while($row = pg_fetch_array( $result )) 
+                    {
+                            // echo out the contents of each row into a table
+                            echo "<tr>";
+                            echo '<td>' . $row['emp_id'] . '</td>';
+                            echo '<td>' . $row['employee_name'] . '</td>';
+                            echo '<td>' . $row['gender'] . '</td>';
+                            echo '<td>' . $row['age'] . '</td>';
+                            echo '<td>' . $row['emp_type'] . '</td>';
+                            echo '<td>' . $row['salary'] . '</td>';
+                            echo '<td>' . $row['contact_no'] . '</td>'.'</tr>';
+                    }
+                    echo "</tbody></table>";
+                ?>
                 <form class="form" action="/Hospital-DBMS/HTML/rec_insert.php" method="POST">
                     <div class="form-group-sm">
                         <label for="emp_id">Employee ID:</label>
@@ -84,13 +143,6 @@ if(isset($_POST["update_info"]))
             </div>
             <div id="med_inv" class="tab-pane fade">
                 <h3>Medicine Inventory</h3>
-                <form class="form" action="/Hospital-DBMS/HTML/rec_insert.php" method="POST">
-                    <div class="form-group-sm">
-                        <label for="med_id">Medicine ID:</label>
-                        <input type="number" class="form-control" id="med_id" name="med_id" required>
-                    </div>
-                    <button type="submit" name="remove_med_inv" class="btn btn-default" value="submit">Submit</button>
-                </form>
                 <?php 
                 $host = "host = localhost";
                 $port = "port = 5432";
@@ -112,7 +164,14 @@ if(isset($_POST["update_info"]))
                         echo '<td>' . $row['cost'] . '</td>'.'</tr>';
                 }
                 echo "</tbody></table>";
-            ?>
+                ?>
+                <form class="form" action="/Hospital-DBMS/HTML/rec_insert.php" method="POST">
+                    <div class="form-group-sm">
+                        <label for="med_id">Medicine ID:</label>
+                        <input type="number" class="form-control" id="med_id" name="med_id" required>
+                    </div>
+                    <button type="submit" name="remove_med_inv" class="btn btn-default" value="submit">Submit</button>
+                </form>
             </div>
         </div>
     </div>
